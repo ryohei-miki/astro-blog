0defb91703fb48760931d9eb406ce5ffe339cb7a
ryohei-miki <rjrbceb101@gmail.com>
2023-03-06 23:44:06 +0900

    feat: tailwind

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
-
 export const Header: React.FC = () => {
-  const [count, setCount] = useState(0);
-
-  const onClickButton = () => {
-    console.log("onclick button");
-    setCount((prev) => prev + 1);
-  };
   return (
-    <div>
-      <button onClick={onClickButton}>Header</button>
-      <div>count{count}</div>
-    </div>
+    <header className="fz p-6 flex">
+      <div className="flex-1 text-4xl font-bold">RM Blog</div>
+      <ul className="flex">
+        <li>GitHub</li>
+        <li>Twitter</li>
+      </ul>
+    </header>
   );
 };
