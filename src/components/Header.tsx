238a43c57964990bc89e969fb1ca9ad51c4ae82a
ryohei-miki <rjrbceb101@gmail.com>
2023-03-07 19:45:40 +0900

    fix: CSSの修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,35 @@
 export const Header: React.FC = () => {
   return (
-    <header className="fz p-6 flex">
+    <header className="flex items-center p-6">
       <div className="flex-1 text-4xl font-bold">RM Blog</div>
-      <ul className="flex">
-        <li>GitHub</li>
-        <li>Twitter</li>
+      <ul className="flex space-x-4 font-semibold">
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer noopener"
+            href="https://ryohei-miki.dev"
+          >
+            Profile
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer noopener"
+            href="https://github.com/ryohei-miki"
+          >
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer noopener"
+            href="https://twitter.com/rm_oshushi"
+          >
+            Twitter
+          </a>
+        </li>
       </ul>
     </header>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
+  theme: {
+    extend: {
+      colors: {
+        purple: "#C778DD",
+        cyan: "#7CDBCB",
+        red: "#E43777",
+        navy: "#574EC1",
+        grey: "#CECECE",
+      }
+    }
+  },
 	plugins: [
 		require('@tailwindcss/line-clamp'),
 	],
